9ee327f228cb340bdab63bbbb29a8c24754dcb3b
ryoma-abe <liaozhenanbu387@gmail.com>
2025-05-03 09:06:26 +0900

    トップページのレイアウトを改善し、コンテナの構造を修正しました。また、Headerコンポーネントを削除しました。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,17 @@ import UserCard from "@/components/User/UserCard";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-10 gap-10 container mx-auto min-h-dvh">
-      <div className="col-span-2 bg-gray-100">
-        <SideBer />
-      </div>
-      <div className="col-span-5">
-        <TimeLine />
-      </div>
-      <div className="col-span-3">
-        <UserCard />
+    <div className="container mx-auto">
+      <div className="grid grid-cols-10 gap-10 min-h-dvh">
+        <div className="col-span-2 bg-gray-100">
+          <SideBer />
+        </div>
+        <div className="col-span-5">
+          <TimeLine />
+        </div>
+        <div className="col-span-3">
+          <UserCard />
+        </div>
       </div>
     </div>
   );
