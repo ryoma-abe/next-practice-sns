c2b96b572806d16d58e10e93e6b38d7b7b8156f7
ryoma-abe <liaozhenanbu387@gmail.com>
2025-05-05 10:43:07 +0900

    LoginFormコンポーネントを削除し、LoginPageでのインポートパスを更新しました。これにより、コードの整理と構造の一貫性が向上しました。

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,8 +1,14 @@
+"use client";
+
+import { useActionState } from "react";
+import { authenticate } from "@/lib/authenticate";
+
 export default function LoginForm() {
+  const [errorMessage, formAction] = useActionState(authenticate, undefined);
   return (
     <div className="bg-white p-8 rounded-xl shadow-md w-full max-w-md">
       <h2 className="text-2xl font-bold text-center mb-6">ログイン</h2>
-      <form className="space-y-4">
+      <form action={formAction} className="space-y-4">
         <div>
           <label
             className="block text-sm font-medium text-gray-700 mb-1"
@@ -13,8 +19,10 @@ export default function LoginForm() {
           <input
             type="email"
             id="email"
+            name="email"
             className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             placeholder="you@example.com"
+            required
           />
         </div>
         <div>
@@ -27,8 +35,10 @@ export default function LoginForm() {
           <input
             type="password"
             id="password"
+            name="password"
             className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             placeholder="••••••••"
+            required
           />
         </div>
         <button
@@ -37,6 +47,17 @@ export default function LoginForm() {
         >
           ログイン
         </button>
+        <div
+          className="flex h-8 items-end space-x-1"
+          aria-live="polite"
+          aria-atomic="true"
+        >
+          {errorMessage && (
+            <div className="text-red-500">
+              <p className="text-sm text-red-500">{errorMessage}</p>
+            </div>
+          )}
+        </div>
       </form>
     </div>
   );
